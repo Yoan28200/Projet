a29566c0d71b438813bc78c2bbd7ddcb05119db5
Yoan28200 <92032378+Yoan28200@users.noreply.github.com>
2022-07-26 16:04:07 +0200

    add text

---
+++ logique.js
@@ -0,0 +1,54 @@
+//----------------------------------------------------------------------------------------------
+
+// création de l'identifiant
+
+// déclaration dotenv
+
+//let user = {
+//     id: 42,
+//     name: 'Yoan',
+//     email: 'yoan.laplace28@gmail.com',
+//     admin: true,
+// };
+
+
+
+//-----------------------------------------------------------------------------------------------
+
+// génération du Token
+
+// déclarer jsonwebtoken
+
+// fonction qui genere le token {
+//     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '24h/48h'});
+// }
+
+//------------------------------------------------------------------------------------------------
+
+// création des routes + verification mot de passe / email
+
+// déclarer express
+
+//  si {identifiant ne correspond pas {     
+        //envoyer message d'erreur 401
+//         sinon { continuer
+//         };   
+//  };
+
+//  si {mdp ne correspond pas {
+        //envoyer message d'erreur 401
+//          sinon { continuer
+//          }   
+//  }
+//     const accessToken = generateAccessToken(user);
+//     res.send({
+//         accessToken,
+//     });
+// });
+
+//--------------------------------------------------------------------------------------------------
+
+// Refresh Token
+
+
+
