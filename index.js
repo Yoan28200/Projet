83d2d396905ac568e555798998b80fa78ab86192
Yoan28200 <92032378+Yoan28200@users.noreply.github.com>
2022-07-28 06:24:56 +0200

    petits avancements code

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('cc')
+const fs = require('fs');
+const bcrypt = require('bcrypt');
+
+const psw = "ergeqrgqrg";
+const username = 'tamer';
+
+bcrypt.hash(psw, 5, function(error, bcryptedPassword) {
+  const newUser = {"username": username, "password" : bcryptedPassword }
+  
+  fs.writeFile('chris',(newUser), function(error){
+    if (error) throw error;
+    console.log('fichier utilisateur crée !'); 
+  })
+})
+
+
+
